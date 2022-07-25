e8870be0465c64d9b2387e5df1b4d7b17be9b8d2
vikash kumar <vikashkumar04081999@gmail.com>
2022-07-25 12:15:53 +0530

    3rd q done

---
+++ q3.js
@@ -0,0 +1,9 @@
+function targetIndex(arr, target) {
+  let newarr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (target == arr[i]) newarr.push(i);
+  }
+  console.log(newarr);
+}
+
+targetIndex([1, 2, 3, 4, 2, 5], 2);

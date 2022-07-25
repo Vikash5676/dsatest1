4be6d40ffaf0e0eaf52a0cf22488f0309b1c8c0b
vikash kumar <vikashkumar04081999@gmail.com>
2022-07-25 11:32:21 +0530

    1st q done

---
+++ q1.js
@@ -0,0 +1,11 @@
+function leaders(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[i + 1]) {
+      console.log(arr[i]);
+      //   first = arr[i];
+    }
+  }
+  console.log(arr[arr.length - 1]);
+}
+
+leaders([1, 2, 3, 8, 6, 5, 4]);
